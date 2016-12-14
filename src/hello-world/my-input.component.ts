e697173683f0e3b9b6b89d092c0f550ebeee3d5a
Nicolas Del Fiorentino <n.delfiorentino@globant.com>
2016-12-14 15:41:38 -0300

    [Nicolas Del Fiorentino] Chapter 1 finished

--- src/hello-world/my-input.component.ts
+++ src/hello-world/my-input.component.ts
@@ -1,23 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'my-input',
     template: `
-        <button (click)="onClick()">Random Number</button>
-        <input [value]="username">
-        {{randomNumber}}
+        <div>
+            <input [(ngModel)]="username">
+            <select [(ngModel)]="city" >
+                <option value=""></option>
+                <option value="Rosario">Rosario</option>
+                <option value="Cordoba">Cordoba</option>
+                <option value="Buenos Aires">Buenos Aires</option>
+            </select>
+        </div>
+        <div>
+        <p> Username: {{ username }} </p>
+        <p> City: {{ city }} </p>
+        </div>
+        <button (click)="myInput.value = 'Hello World'">Click me</button>
+        <input #myInput>
+        <div *ngFor="let num of numbers" >
+        {{ num }}
+        </div>
     `,
     directives: [],
     providers: []
 })
 class MyInputComponent {
-    username = 'Jesse';
-    randomNumber = 3; // used a dice
-
-    onClick(event) {
-        this.randomNumber = Math.random() * 10;
-    }
-
+    username: string = 'Jss';
+    city: string = ''; // No City at first*/
+    numbers = [1, 2, 3, 4, 5];
 }
 
 export default MyInputComponent;
